fb2c495298288f0f32a51fe2b506eed0534a6e53
hemendra <hemendra2k1@gmail.com>
2026-02-18 16:12:48 +0530

    Updated dbt_project.yml with view materialization for staging models

--- blood_bank_management_system/models/staging/stg_donors.sql
+++ blood_bank_management_system/models/staging/stg_donors.sql
@@ -1,1 +1,18 @@
-SELECT * FROM {{ source('raw', 'donors') }}
+
+SELECT 
+    donor_id int,
+    name::varchar as donor_name,
+    age::int as donor_age,
+    gender:: varchar,
+    weight:: numeric(5,2),
+    blood_group::varchar,
+    last_donation_date::date,
+    case 
+        when is_eligible = '1' then true 
+        else false 
+    end as is_eligible,
+    donations_count::int, 
+    last_health_check_date:: date as last_health_checkup_date,
+    location::varchar as donor_location
+
+FROM {{ source('raw', 'donors') }}
