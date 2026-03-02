155ec6915a6f5a52a88ba1f12a44880e58f30dd1
hemendra <hemendra2k1@gmail.com>
2026-03-02 12:41:44 +0530

    Created blood inventory fact with merge logic

---
+++ blood_bank_management_system/models/analytics/facts/fct_blood_inventory.sql
@@ -0,0 +1,26 @@
+{{ config(
+        materialized='incremental',
+        unique_key='inventory_id',
+        incremental_strategy='merge'
+    )
+}}
+
+with blood_inventory_source as (
+    select * from {{ ref('stg_blood_inventory') }}
+)
+
+
+select 
+    bis.inventory_id,
+    bis.donation_id,
+    bis.blood_group,
+    ddt_received.date_sk as received_date_sk,
+    ddt_expiration.date_sk as expiration_date_sk,
+    bis.units_available,
+    bis.volume,
+    bis.quality,
+    status
+from blood_inventory_source bis
+
+left join {{ ref('dim_date') }} ddt_received on bis.date_received = ddt_received.full_date
+left join {{ ref('dim_date') }} ddt_expiration on bis.expiration_date = ddt_expiration.full_date
