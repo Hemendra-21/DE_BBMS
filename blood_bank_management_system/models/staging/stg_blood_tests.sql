00ca7444d4b0c4374ef1a649b39eddbc6cb123d0
hemendra <hemendra2k1@gmail.com>
2026-03-03 13:29:04 +0530

    Updated stg_blood_inventory.yml

--- blood_bank_management_system/models/staging/stg_blood_tests.sql
+++ blood_bank_management_system/models/staging/stg_blood_tests.sql
@@ -1,11 +1,26 @@
-select 
-    test_id::int as test_id,
-    donor_id::int as donor_id,
-    technician_id::int as technician_id,
-    nullif(date, '0000-00-00')::date as test_date,
-    disease_tested::varchar as disease_tested,
-    result::varchar as result,
-    test_type::varchar as test_type,
-    comments::varchar as comments
+with source as (
+    select * from {{ source('raw', 'blood_tests') }}
+),
 
-FROM {{ source('raw', 'blood_tests') }}
+cleaned as (
+
+    select
+        test_id::int as test_id,
+        donor_id::int as donor_id,
+        technician_id::int as technician_id,
+
+        case
+            when date = '0000-00-00' then null
+            else date::date
+        end as test_date,
+
+        nullif(trim(disease_tested), '')  as disease_tested,
+        result::varchar as result,
+        trim(test_type) as test_type,
+        nullif(trim(comments), '') as comments
+
+    from source
+
+)
+
+select * from cleaned
