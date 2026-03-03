26931383cad9af26ea6476bb55838b89df763b3b
hemendra <hemendra2k1@gmail.com>
2026-03-03 16:00:42 +0530

    Updated stg_technicians and implemented tests

--- blood_bank_management_system/models/staging/stg_technicians.sql
+++ blood_bank_management_system/models/staging/stg_technicians.sql
@@ -1,14 +1,21 @@
+with source as (
+    select * from {{ source('raw', 'technicians')}}
+), 
 
+cleaned as (
+    select
+        technician_id::int as technician_id,
+        hospital_id::int as hospital_id,
+        assigned_hospital_id::int as assigned_hospital_id,
 
-SELECT
-    technician_id::int as technician_id,
-    hospital_id::int as hospital_id,
-    assigned_hospital_id::int as assigned_hospital_id,
+        initcap(trim(name))::varchar as technician_name,
+        initcap(trim(qualification))::varchar as qualification,
 
-    initcap(trim(name))::varchar as technician_name,
-    initcap(trim(qualification))::varchar as qualification,
+        trim(phone_number)::varchar as phone_number,
+        lower(trim(email_address))::varchar as email_address,
 
-    trim(phone_number)::varchar as phone_number,
-    lower(trim(email_address))::varchar as email_address
+        ingested_at::timestamp as ingested_at
+    from source
+)
 
-FROM {{source('raw', 'technicians')}}
+select * from cleaned
