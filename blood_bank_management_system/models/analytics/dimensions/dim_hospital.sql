111ffb30218d6064023632910898f0b0243d3b09
hemendra <hemendra2k1@gmail.com>
2026-03-03 19:51:39 +0530

    Updated dimensions

--- blood_bank_management_system/models/analytics/dimensions/dim_hospital.sql
+++ blood_bank_management_system/models/analytics/dimensions/dim_hospital.sql
@@ -16,8 +16,12 @@ final as (
         phone_number,
         hospital_type,
         accreditation_status,
-        dbt_valid_from,
-        dbt_valid_to 
+        dbt_valid_from as valid_from,
+        dbt_valid_to as valid_to,
+        case 
+            when dbt_valid_to is null then true 
+            else false
+        end as is_current
     from snap
 )
 
--- blood_bank_management_system/models/analytics/dimensions/dim_recipient.sql
+++ blood_bank_management_system/models/analytics/dimensions/dim_recipient.sql
@@ -14,8 +14,12 @@ final as (
         recipient_name,
         blood_group as recipient_blood_group,
         recipient_location,
-        dbt_valid_from,
-        dbt_valid_to
+        dbt_valid_from as valid_from,
+        dbt_valid_to as valid_to,
+        case 
+            when dbt_valid_to is null then true
+            else false 
+        end as is_current
     from snap        
 )
 
