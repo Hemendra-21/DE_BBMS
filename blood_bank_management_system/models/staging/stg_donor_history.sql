5773089302fa445ac47a740669fced36715a9f58
hemendra <hemendra2k1@gmail.com>
2026-03-03 15:04:47 +0530

    Updated stg_donor_history and implemented tests

--- blood_bank_management_system/models/staging/stg_donor_history.sql
+++ blood_bank_management_system/models/staging/stg_donor_history.sql
@@ -1,8 +1,16 @@
+with source as (
+    select * from {{ source('raw', 'donor_history') }}
+),
 
-SELECT
-    history_id::int as history_id,
-    donor_id::int as donor_id,
-    donation_id::int as donation_id,
-    lower(trim(reaction))::varchar as reaction,
-    notes::varchar as notes
-FROM {{source('raw', 'donor_history')}}
+cleaned as (
+    select
+        history_id::int as donor_history_id,
+        donor_id::int as donor_id,
+        donation_id::int as donation_id,
+        lower(trim(reaction))::varchar as reaction,
+        notes::varchar as notes,
+        ingested_at::timestamp as ingested_at
+    from source
+)
+
+select * from cleaned
