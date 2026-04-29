d2b07b1819446eca65c4b3e65cfeff5ab7ef540b
Hemendra-21 <hemendra2k1@gmail.com>
2026-04-30 03:41:54 +0530

    Cleanup: remove duplicate root-level dbt files

--- blood_bank_management_system/models/reports/2_Donor_Demographics.sql
+++
@@ -1,7 +0,0 @@
-select
-    donor_blood_group,
-    count(*) as total_donors,
-    rank() over (order by count(*) desc) as donor_rank
-from {{ ref('dim_donor') }}
-group by donor_blood_group
-order by donor_rank
